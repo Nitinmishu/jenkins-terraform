9503ff7499c4a0ee78e694253e09bf4712a11454
nitin <nisingh006@gmail.com>
2021-11-09 16:32:58 +0000

    file to get all ec2

--- getec2ip.py
+++ getec2ip.py
@@ -1,3 +1,4 @@
+
 import boto3
 
 ec2 = boto3.resource('ec2',region_name='us-east-1')
@@ -6,14 +7,18 @@ def lambda_handler():
     # create filter for instances in running state
     filters = [
         {
-            'Name': 'instance-state-name', 
+            'Name':'tag:Name',
+            'Values':['my-ec2-instance']
+        },
+        {
+            'Name': 'instance-state-name',
             'Values': ['running']
         }
     ]
-    
+
     # filter the instances based on filters() above
     instances = ec2.instances.filter(Filters=filters)
-
+    #import pdb; pdb.set_trace()
     # instantiate empty array
     RunningInstances = []
 
@@ -21,8 +26,8 @@ def lambda_handler():
         print(instance.__dict__)
         # for each instance, append to array and print instance id
         RunningInstances.append(instance.id)
-        print (instance.id)
-        
+        print (instance.id,instance.public_ip_address,instance.private_ip_address)
+
 lambda_handler()
 
 print("this is python ec2")
